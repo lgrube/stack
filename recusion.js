ac2f540355ec11d23cc2490854416842b1b0d686
lgrube <lgv9483@saintjoe.edu>
2015-01-26 19:45:30 -0600

    recursion file

---
+++ recusion.js
@@ -0,0 +1,29 @@
+//luke grube
+//1/26/15
+//recursion code
+//recursively run through the stack
+load("stack.js");
+
+function factorial(n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * factorial(n-1);
+	}
+}
+
+function fact(n) {
+	var s = new Stack();
+	while (n > 1) {
+		s.push(n--);
+	}
+	var product = 1;
+	while (s.lenght() > 0) {
+		product *= s.pop();
+	}
+	return product;
+}
+
+print(factorial(5));
+print(fact(5));
